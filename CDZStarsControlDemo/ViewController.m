e103e2d11bba94d5a32cf526d205e4c67f7ad7f2
依萌匠心网络部 <YiMengZhang@yimengjiangxinwangluobudeimac.local>
2018-04-16 11:54:57 +0800

    Initial Commit

---
+++ CDZStarsControlDemo/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  CDZStarsControlDemo
+//
+//  Created by Nemocdz on 2017/5/20.
+//  Copyright © 2017年 Nemocdz. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CDZStarsControl.h"
+
+@interface ViewController ()<CDZStarsControlDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (nonatomic, strong) CDZStarsControl *starsControl;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view addSubview:self.starsControl];
+    self.label.text = [NSString stringWithFormat:@"%.1f",self.starsControl.score];
+}
+
+- (void)starsControl:(CDZStarsControl *)starsControl didChangeScore:(CGFloat)score{
+    self.label.text = [NSString stringWithFormat:@"%.1f",score];
+}
+
+
+- (CDZStarsControl *)starsControl{
+    if (!_starsControl) {
+        _starsControl = [CDZStarsControl.alloc initWithFrame:CGRectMake(10, 100, self.view.frame.size.width - 20 , 50) stars:5 starSize:CGSizeMake(50, 50) noramlStarImage:[UIImage imageNamed:@"star_normal"] highlightedStarImage:[UIImage imageNamed:@"star_highlighted"]];
+        _starsControl.delegate = self;
+        _starsControl.allowFraction = YES;
+        _starsControl.score = 1.8f;
+    }
+    return _starsControl;
+}
+
+@end
